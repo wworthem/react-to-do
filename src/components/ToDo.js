940084db35975e53171d9e17a73883f775795084
Warren <warrenworthem46@gmail.com>
2018-04-15 19:48:55 -0400

    Completed work: checkpt-react-state and props

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 class ToDo extends Component {
   render() {
     return (
-      <li>To Do list goes here!</li>
+  //    <li>To Do list goes here!</li>
+      <li>{this.props.description}</li>
           );
   }
 }
