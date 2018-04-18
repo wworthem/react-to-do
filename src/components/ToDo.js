9b0fd0a64b1cef19c3fad13254d44eb9e70f4c69
Warren <warrenworthem46@gmail.com>
2018-04-18 15:12:32 -0400

    checkpoint react to-do events completed

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -6,7 +6,7 @@ class ToDo extends Component {
   //    <li>To Do list goes here!</li>
   //    <li>{this.props.description}</li>
       <li>
-        <input type="checkbox" checked={ this.props.isCompleted } />
+        <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
         <span>{this.props.description }</span>
       </li>
   );
